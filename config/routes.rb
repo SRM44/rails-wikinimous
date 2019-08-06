d1c251f86326893ab1a8e184f7879cc09b48ba11
Steven Rigolet <steven-r@hotmail.fr>
2019-08-06 16:11:52 +0200

    wiki is set up and full operational

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'articles', to: 'articles#index'
+
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+
+  get 'articles/:id', to: 'articles#show', as: :article
+
+  get 'articles/:id/edit', to: 'articles#edit'
+  patch 'articles/:id', to: 'articles#update'
+
+  delete 'articles/:id', to: 'articles#destroy', as: :delete
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
